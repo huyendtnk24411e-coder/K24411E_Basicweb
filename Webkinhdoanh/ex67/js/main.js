3c533b54bd7b4d0f523426fbe5c2ad975355f941
Ngoc Huyen <huyendtnk24411e@st.uel.edu.vn>
2026-06-17 11:56:29 +0700

    commit

--- Webkinhdoanh/ex67/js/main.js
+++ Webkinhdoanh/ex67/js/main.js
@@ -1,40 +1,31 @@
 function load_students(student_xml_text, bodyStudent)
 {
     var parser = new DOMParser();
-    var xmlDoc = parser.parseFromString(student_xml_text,"text/xml");
-
-    var tag_student = xmlDoc.getElementsByTagName("student");
-
-    for(let i=0;i<tag_student.length;i++)
+    var xmlDoc = parser.parseFromString(student_xml_text, "text/xml");
+    var students = xmlDoc.getElementsByTagName("student");
+    for(var i = 0; i < students.length; i++)
     {
-        let id =
-        tag_student[i].getElementsByTagName("id")[0].childNodes[0].nodeValue;
-
-        let name =
-        tag_student[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-
-        let birthday =
-        tag_student[i].getElementsByTagName("birthday")[0].childNodes[0].nodeValue;
-
-        let gender =
-        tag_student[i].getElementsByTagName("gender")[0].childNodes[0].nodeValue;
-
-        let tr = document.createElement("tr");
-
-        tr.innerHTML = `
-            <td>${id}</td>
-            <td>${name}</td>
-            <td>${birthday}</td>
-            <td>${gender}</td>
-        `;
-
-        tr.onclick = function(){
-            document.getElementById("sid").innerHTML = id;
-            document.getElementById("sname").innerHTML = name;
-            document.getElementById("sbirthday").innerHTML = birthday;
-            document.getElementById("sgender").innerHTML = gender;
-        }
-
-        bodyStudent.appendChild(tr);
+        var id =
+        students[i].getElementsByTagName("id")[0].childNodes[0].nodeValue;
+        var name = students[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+        var birthday = students[i].getElementsByTagName("birthday")[0].childNodes[0].nodeValue;
+        var gender = students[i].getElementsByTagName("gender")[0].childNodes[0].nodeValue;
+        var row = document.createElement("tr");
+        row.innerHTML =
+        "<td>" + id + "</td>" +
+        "<td>" + name + "</td>" +
+        "<td>" + birthday + "</td>" +
+        "<td>" + gender + "</td>";
+        row.onclick = function(studentId, studentName, studentBirthday, studentGender)
+        {
+            return function()
+            {
+                document.getElementById("sid").innerHTML = studentId;
+                document.getElementById("sname").innerHTML = studentName;
+                document.getElementById("sbirthday").innerHTML = studentBirthday;
+                document.getElementById("sgender").innerHTML = studentGender;
+            }
+        }(id, name, birthday, gender);
+        bodyStudent.appendChild(row);
     }
 }
